0b327531cb15a52b03799cbf28cf398d38236141
heronsbillC <1907429529@qq.com>
2018-11-23 17:28:28 +0800

    管理员与普通用户权限还未解决，登录之后貌似没有进行权限检测。

--- src/main/java/com/clubfactory/demo/test/config/SecurityConfig.java
+++ src/main/java/com/clubfactory/demo/test/config/SecurityConfig.java
@@ -3,29 +3,48 @@ package com.clubfactory.demo.test.config;
 import com.clubfactory.demo.test.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)//开启security注解
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/admin").access("hasRole('ADMIN')")
-                    .antMatchers("/index").access("hasRole('USER')")
-                    .antMatchers("/hello").permitAll()  //不需要权限也可以访问
+
+        //允许所有用户访问"/"和"/home"
+        http.authorizeRequests()
+                .antMatchers( "/LoginController/home","/LoginController/loginAction").permitAll()
+                .antMatchers("/LoginController/admin").access("hasRole('ADMIN')")
+                .antMatchers("/LoginController/user").access("hasRole('USER')")
+                //其他地址的访问均需验证权限
+//                .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .usernameParameter("username")     //指定页面中对应用户名的参数名称
-                    .passwordParameter("password")     //指定页面中对应密码的参数名称
-                    .defaultSuccessUrl("/hello")
-                    .failureUrl("/error");
+                //指定登录页是"/login"
+                .loginPage("/LoginController/login")
+                .defaultSuccessUrl("/LoginController/hello")//登录成功后默认跳转到"/hello"
+                .permitAll()
+                .usernameParameter("username")     //指定页面中对应用户名的参数名称
+                .passwordParameter("password")     //指定页面中对应密码的参数名称
+                .and()
+                .logout()
+                .logoutSuccessUrl("/LoginController/home")//退出登录后的默认url是"/home"
+                .permitAll()
+                .and()
+                .csrf().disable();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring() //ignoring它是完全绕过spring security的所有filter的；根本不会经过springSecurity的任何拦截，所以适合静态资源的放过
+                .antMatchers("/LoginController/loginAction");
     }
 
     /**
--- src/main/java/com/clubfactory/demo/test/controller/LoginController.java
+++ src/main/java/com/clubfactory/demo/test/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.clubfactory.demo.test.controller;
 
 import com.clubfactory.demo.test.pojo.Users;
-import com.clubfactory.demo.test.service.UserService;
 import com.clubfactory.demo.test.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -18,11 +20,12 @@ public class LoginController {
     @Resource
     private UserServiceImpl userService;
 
-    @RequestMapping("/loginAction")
+    @PostMapping("/loginAction")
     public String loginAction(HttpServletRequest request){
+        System.out.println("********************");
         String name = request.getParameter("username");
         String password = request.getParameter("password");
-        System.out.println("userName=" + name + "/tpassword=" + password);
+        System.out.println("userName=" + name + "   password=" + password);
         Users user = userService.login(name,password);
         log.info("LoginController.loginAction->user="+user);
         if(user != null)
@@ -31,19 +34,25 @@ public class LoginController {
             return "error";
     }
 
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
 
-    @RequestMapping("/admin")
+//    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(value = "/admin")
     public String admin() {
         return "admin";
     }
 
-    @RequestMapping("/index")
-    public String index() {
-        return "index";
+//    @PreAuthorize("hasRole('USER')")
+    @RequestMapping(value = "/user")
+    public String user() {
+        return "user";
     }
 
     @RequestMapping("/login")
